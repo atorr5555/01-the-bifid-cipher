38603d407fbc6164154042791b7183eab993d686
atorr5555 <31078782+atorr5555@users.noreply.github.com>
2021-09-20 00:02:26 -0500

    Caso decrypt

--- program.py
+++ program.py
@@ -1,23 +1,6 @@
 import fileinput
 import string
 
-KEY='ENCRYPT'
-action=''
-phrase=''
-alphabet = list(string.ascii_uppercase.replace('J',''))
-for char in KEY:
-	alphabet.remove(char)
-
-count = 0
-for line in fileinput.input():
-	if count == 0:
-		action = line
-		count += 1
-		continue
-	if count > 1:
-		break
-	phrase = line
-
 def fill_table():
 	i=0
 	j=0
@@ -46,27 +29,61 @@ def find_char(char):
 				return (i, j)
 	
 
+if __name__ == "__main__":
+	KEY='ENCRYPT'
+	action=''
+	phrase=''
+	alphabet = list(string.ascii_uppercase.replace('J',''))
+	for char in KEY:
+		alphabet.remove(char)
+	count = 0
+	for line in fileinput.input():
+		if count == 0:
+			action = line
+			count += 1
+			continue
+		if count > 1:
+			break
+		phrase = line
+	table = [ [ 'A' for i in range(5) ]  for i in range(5) ]
+	fill_table()
+	phrase = phrase.replace(' ', '')
+	phrase = phrase.replace('\n', '')
+	
+	aux = []
+	for char in phrase:
+		aux.append(find_char(char))
+	
+	if action == 'ENCRYPT':
 
-table = [ [ 'A' for i in range(5) ]  for i in range(5) ]
-fill_table()
-phrase = phrase.replace(' ', '')
-phrase = phrase.replace('\n', '')
-aux = []
-for char in phrase:
-	aux.append(find_char(char))
+		string_nums = ''
+		for x in aux:
+			string_nums += str(x[0])
 
-string_nums = ''
-for x in aux:
-	string_nums += str(x[0])
+		for x in aux:
+			string_nums += str(x[1])
 
-for x in aux:
-	string_nums += str(x[1])
+		nuevas_coords = []
+		for i in range(0, len(string_nums), 2):
+			nuevas_coords.append((int(string_nums[i]), int(string_nums[i+1])))
 
-nuevas_coords = []
-for i in range(0, len(string_nums), 2):
-	nuevas_coords.append((int(string_nums[i]), int(string_nums[i+1])))
+		cifrado=''
+		for x in nuevas_coords:
+			cifrado += table[x[0]][x[1]]
+		print(cifrado)
+	else:
+		string_nums = ''
+		for x in aux:
+			string_nums += str(x[0])
+			string_nums += str(x[1])
+		
+		break_point = int(len(string_nums) / 2)
+		parte1 = string_nums[:break_point]
+		parte2 = string_nums[break_point:]
 
-cifrado=''
-for x in nuevas_coords:
-	cifrado += table[x[0]][x[1]]
-print(cifrado)
+		coord_origen = [(int(parte1[i]), int(parte2[i])) for i in range(break_point)]
+
+		original = ''
+		for x in coord_origen:
+			original += table[x[0]][x[1]]
+		print(original)
